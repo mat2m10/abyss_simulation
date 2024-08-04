ac732ab882d74a0aae9fde94df2412bd3b9ab2eb
matthieu de hemptinne <matthieu.dehemptinne@gmail.com>
2024-08-04 20:30:59 +0200

    f

--- models.py
+++ models.py
@@ -160,7 +160,6 @@ def manhattan_linear(geno, y, covs=None):
                 # check if you have snp specific covariates
                 for snp in list(geno.columns):
                     X = geno[snp]
-                    print(covs[f"{snp}"])
                     betas, p_values = ols_regression(y, X, covs[f"{snp}"])
                     coefs.append(betas[snp])
                     Ps.append(p_values[snp])
